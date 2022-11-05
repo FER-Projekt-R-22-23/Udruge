3753e0ed4fea635a42aea996368e1e1a2f409e63
Sarmaguy <64214723+Sarmaguy@users.noreply.github.com>
2022-11-05 17:07:29 +0100

    Morao sam ispravit ije/je :')

--- UdrugeApp/UdrugeApp.Domain/Models/Prostori.cs
+++ UdrugeApp/UdrugeApp.Domain/Models/Prostori.cs
@@ -8,17 +8,17 @@ public class Prostori
 	private int _IdUdruge;
     private string _Adresa;
     private string _Namjena;
-    private string _Dodjelio;
+    private string _Dodijelio;
     private DateTime? _DodjeljenoDo;
 
     public int Id { get => _Id; set => _Id = value; }
     public int IdUdruge { get => _IdUdruge; set => _IdUdruge = value; }
     public string Adresa { get => _Adresa; set => _Adresa = value; }
     public string Namjena { get => _Namjena; set => _Namjena = value; }
-    public string Dodjelio { get => _Dodjelio; set => _Dodjelio = value; }
+    public string Dodijelio { get => _Dodijelio; set => _Dodijelio = value; }
     public DateTime? DodjeljenoDo { get => _DodjeljenoDo; set => _DodjeljenoDo = value; }
 
-    public Prostori(int id, int idUdruge, string Adresa, string Namjena, string Dodjelio, DateTime? DodjeljenoDo)
+    public Prostori(int id, int idUdruge, string Adresa, string Namjena, string Dodijelio, DateTime? DodjeljenoDo)
     {
         if (string.IsNullOrEmpty(Adresa))
         {
@@ -37,16 +37,16 @@ public class Prostori
         {
             throw new ArgumentException($"'{nameof(idUdruge)}' cannot be null or empty.", nameof(idUdruge));
         }
-        if (string.IsNullOrEmpty(Dodjelio))
+        if (string.IsNullOrEmpty(Dodijelio))
         {
-            throw new ArgumentException($"'{nameof(Dodjelio)}' cannot be null or empty.", nameof(Dodjelio));
+            throw new ArgumentException($"'{nameof(Dodijelio)}' cannot be null or empty.", nameof(Dodijelio));
         }
 
         _Id = id;
         _IdUdruge = idUdruge;
         _Adresa = Adresa;
         _Namjena = Namjena;
-        _Dodjelio = Dodjelio;
+        _Dodijelio = Dodijelio;
         _DodjeljenoDo = DodjeljenoDo;
     }
 
@@ -58,20 +58,20 @@ public class Prostori
                _IdUdruge == Prostori._IdUdruge &&
                _Adresa == Prostori._Adresa &&
                _Namjena == Prostori._Namjena &&
-               _Dodjelio == Prostori._Dodjelio &&
+               _Dodijelio == Prostori._Dodijelio &&
                _DodjeljenoDo == Prostori._DodjeljenoDo
     }
 
     public override int GetHashCode()
     {
-        return HashCode.Combine(_Id,_IdUdruge, _Adresa, _Namjena, _Dodjelio, _DodjeljenoDo);
+        return HashCode.Combine(_Id,_IdUdruge, _Adresa, _Namjena, _Dodijelio, _DodjeljenoDo);
     }
 
     public override Result IsValid()
         => Validation.Validate(
             (() => _Adresa.Length <= 50, "Adresa lenght must be less than 50 characters"),
             (() => _Namjena.Length <= 50, "Namjena lenght must be less than 100 characters"),
-            (() => _Dodjelio.Length <= 50, "Dodjelio lenght must be less than 50 characters"),
+            (() => _Dodijelio.Length <= 50, "Dodijelio lenght must be less than 50 characters"),
             (() => !string.IsNullOrEmpty(_Adresa.Trim()), "Adresa can't be null, empty, or whitespace"),
             (() => !string.IsNullOrEmpty(_Namjena.Trim()), "Namjena can't be null, empty, or whitespace")
             );
