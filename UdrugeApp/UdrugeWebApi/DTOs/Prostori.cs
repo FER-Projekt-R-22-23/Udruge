61d68e967c839a8aa837f4cf6d8d3c662731fe4a
mlovric <ml52972@fer.hr>
2022-11-07 17:45:33 +0100

    Fixed prostori DTO

--- UdrugeApp/UdrugeWebApi/DTOs/Prostori.cs
+++ UdrugeApp/UdrugeWebApi/DTOs/Prostori.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using DomainModels = UdrugeApp.Domain.Models;
 
-namespace ExampleWebApi.DTOs;
+namespace UdrugeWebApi.DTOs;
 
 public class Prostori
 {
